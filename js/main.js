0139b708d563c317c6fba93fb90e4144525ea165
= <=>
2018-10-04 00:39:08 +0700

    4/10/2018

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
-	console.log('1');
+	var fol=$('.fol').offset().top;
+	var tag=1
+	//scroll to element
+	$(window).scroll(function(){
+		if($('html,body').scrollTop()>=fol-60)
+		{
+			if(tag==1)
+			{
+				$('.fol').addClass('test');
+				$('.scroll-top').css({"display":"block"});
+				tag=0;
+			}
+
+		}
+		else
+			{
+				if(tag==0)
+				{
+					$('.fol').removeClass('test');
+					$('.scroll-top').css({"display":"none"});
+					tag=1;
+				}	
+
+			}
+
+			
+
+	});
+	//scroll to Top
+	$('.scroll-top').click(function(){
+		console.log('ok click');
+		$('html,body').animate({scrollTop:0},400);
+	});
+	
 })
